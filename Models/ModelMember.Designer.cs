2f28392357ccbb948440ea8e15760963afac0a3b
rmestizor <rmestizor@sspver.gob.mx>
2026-06-01 12:41:17 -0600

    fix: corrige configuracion framework y estabiliza PAISSP

--- Models/ModelMember.Designer.cs
+++ Models/ModelMember.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\jsantamariac\Documents\Visual Studio 2022\Projects\AdmonUser\Models\ModelMember.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Proyectos\AdmonUser\Models\ModelMember.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general sobre un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie los valores de estos atributos para modificar la información
 // asociada a un ensamblado.
-[assembly: AssemblyTitle("AdmonUser")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("PAISSP")]
+[assembly: AssemblyDescription("Portal de Administración Institucional SSPVER")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AdmonUser")]
+[assembly: AssemblyProduct("PAISSP")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión 
 // mediante el carácter '*', como se muestra a continuación:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.0.2")]
+[assembly: AssemblyFileVersion("1.2.0.2")]
